0f9d07897f10835837a6a5b61d4b0bd3fb081097
srivani.muddineni  <srivani.muddineni@hmcts.net>
2026-02-10 19:56:39 +0000

    Added improvements for stubs, callback wiremock

--- src/test/java/uk/gov/hmcts/cp/subscription/integration/IntegrationTestBase.java
+++ src/test/java/uk/gov/hmcts/cp/subscription/integration/IntegrationTestBase.java
@@ -8,6 +8,7 @@ import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+
 import uk.gov.hmcts.cp.openapi.model.ClientSubscriptionRequest;
 import uk.gov.hmcts.cp.openapi.model.NotificationEndpoint;
 import uk.gov.hmcts.cp.subscription.integration.config.TestContainersInitialise;
@@ -23,6 +24,13 @@ import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
 import static uk.gov.hmcts.cp.openapi.model.EventType.CUSTODIAL_RESULT;
 import static uk.gov.hmcts.cp.openapi.model.EventType.PRISON_COURT_REGISTER_GENERATED;
 
@@ -31,6 +39,15 @@ import static uk.gov.hmcts.cp.openapi.model.EventType.PRISON_COURT_REGISTER_GENE
 @AutoConfigureMockMvc
 @Slf4j
 public abstract class IntegrationTestBase {
+
+    protected static final String MATERIAL_METADATA_RESPONSE_PATH = "wiremock/material-client/__files/material-response.json";
+    protected static final String MATERIAL_CONTENT_RESPONSE_PATH = "wiremock/material-client/__files/material-with-contenturl.json";
+    protected static final String MATERIAL_PDF_PATH = "wiremock/material-client/__files/material-content.pdf";
+    protected static final String CALLBACK_RESPONSE_PATH = "wiremock/callback-client/__files/callback-accepted.json";
+    protected static final String MATERIAL_URI = "/material-query-api/query/api/rest/material/material/";
+    public static final String METADATA = "/metadata";
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String CONTENT_TYPE = "Content-Type";
 
     @Resource
     protected MockMvc mockMvc;
@@ -90,5 +107,52 @@ public abstract class IntegrationTestBase {
 
     protected String loadPayload(String path) throws IOException {
         return new ClassPathResource(path).getContentAsString(StandardCharsets.UTF_8);
+    }
+
+    protected void stubMaterialMetadata(UUID materialId) throws IOException {
+        String materialPath = MATERIAL_URI + materialId;
+        String metadataBody = new ClassPathResource(MATERIAL_METADATA_RESPONSE_PATH).getContentAsString(StandardCharsets.UTF_8);
+        stubFor(get(urlPathMatching(".*" + materialPath + METADATA))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON)
+                        .withBody(metadataBody)));
+    }
+
+    protected void stubMaterialContent(UUID materialId) throws IOException {
+        String materialPath = MATERIAL_URI + materialId;
+        String contentBody = new ClassPathResource(MATERIAL_CONTENT_RESPONSE_PATH).getContentAsString(StandardCharsets.UTF_8);
+        stubFor(get(urlPathMatching(".*" + materialPath + "/content"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader(CONTENT_TYPE, "application/vnd.material.query.material+json")
+                        .withBody(contentBody)
+                        .withTransformers("response-template")));
+    }
+
+    protected void stubMaterialBinary(UUID materialId) throws IOException {
+        String materialPath = MATERIAL_URI + materialId;
+        byte[] pdfBody = new ClassPathResource(MATERIAL_PDF_PATH).getContentAsByteArray();
+        stubFor(get(urlPathMatching(".*" + materialPath + "/binary"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader(CONTENT_TYPE, "application/pdf")
+                        .withHeader("Content-Disposition", "inline; filename=\"material-content.pdf\"")
+                        .withBody(pdfBody)));
+    }
+
+    protected void stubMaterialMetadataNoContent(UUID materialId) {
+        String materialPath = MATERIAL_URI + materialId;
+        stubFor(get(urlPathMatching(".*" + materialPath + METADATA))
+                .willReturn(aResponse().withStatus(204)));
+    }
+
+    protected void stubCallbackEndpoint(WireMockServer server, String callbackUri) throws IOException {
+        String body = new ClassPathResource(CALLBACK_RESPONSE_PATH).getContentAsString(StandardCharsets.UTF_8);
+        server.stubFor(post(urlPathEqualTo(callbackUri))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON)
+                        .withBody(body)));
     }
 }
--- src/test/java/uk/gov/hmcts/cp/subscription/integration/e2e/NotificationPCRE2EIntegrationTest.java
+++ src/test/java/uk/gov/hmcts/cp/subscription/integration/e2e/NotificationPCRE2EIntegrationTest.java
@@ -1,32 +1,42 @@
 package uk.gov.hmcts.cp.subscription.integration.e2e;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.wiremock.spring.ConfigureWireMock;
 import org.wiremock.spring.EnableWireMock;
+import org.wiremock.spring.InjectWireMock;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
 
 import uk.gov.hmcts.cp.material.openapi.api.MaterialApi;
-import uk.gov.hmcts.cp.openapi.model.PcrEventPayload;
+import uk.gov.hmcts.cp.subscription.config.SSLTrustingRestTemplateConfig;
 import uk.gov.hmcts.cp.subscription.integration.IntegrationTestBase;
-import uk.gov.hmcts.cp.subscription.model.EntityEventType;
-import uk.gov.hmcts.cp.subscription.services.CallbackDeliveryService;
-import uk.gov.hmcts.cp.subscription.services.DocumentService;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static java.util.Objects.nonNull;
+import static org.awaitility.Awaitility.await;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+
+import org.springframework.test.web.servlet.ResultActions;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -35,39 +45,44 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-// TODO - comments can be removed after handover to QA
-
-@EnableWireMock({@ConfigureWireMock(name = "material-client", baseUrlProperties = "material-client.url", port = 0)})
+@EnableWireMock({
+        @ConfigureWireMock(name = "material-client", baseUrlProperties = "material-client.url", port = 0),
+        @ConfigureWireMock(name = "callback-client", httpsBaseUrlProperties = "callback-client.url", httpsPort = 0)
+})
 @TestPropertySource(properties = {
         "material-client.retry.timeoutMilliSecs=500",
         "material-client.retry.intervalMilliSecs=100"
 })
+@Import(SSLTrustingRestTemplateConfig.class)
 class NotificationPcrE2EIntegrationTest extends IntegrationTestBase {
 
+    private UUID subscriptionId;
+    private UUID callbackDocumentId;
+    private static final UUID MATERIAL_ID = UUID.fromString("6c198796-08bb-4803-b456-fa0c29ca6021");
+    private static final UUID MATERIAL_ID_TIMEOUT = UUID.fromString("11111111-1111-1111-1111-111111111112");
     private static final String NOTIFICATIONS_PCR_URI = "/notifications/pcr";
     private static final String CLIENT_SUBSCRIPTIONS_URI = "/client-subscriptions";
+    private static final String CALLBACK_URI = "/callback/notify";
     private static final String DOCUMENT_URI = CLIENT_SUBSCRIPTIONS_URI + "/{clientSubscriptionId}/documents/{documentId}";
     private static final String SUBSCRIPTION_REQUEST_E2E = "stubs/requests/subscription/subscription-pcr-request.json";
     private static final String PCR_EVENT_PAYLOAD_PATH = "stubs/requests/progression-pcr/pcr-request-prison-court-register.json";
     private static final String PCR_EVENT_TIMEOUT_PATH = "stubs/requests/progression-pcr/pcr-request-material-timeout.json";
-    private static final UUID MATERIAL_ID = UUID.fromString("6c198796-08bb-4803-b456-fa0c29ca6021");
-    private static final UUID MATERIAL_ID_TIMEOUT = UUID.fromString("11111111-1111-1111-1111-111111111112");
 
-    @MockitoBean
-    private CallbackDeliveryService callbackDeliveryService;
+    @InjectWireMock("callback-client")
+    private WireMockServer callbackWireMock;
+
+    @Value("${callback-client.url}")
+    private String callbackHttpsBaseUrl;
 
     @MockitoSpyBean
     private MaterialApi materialApi;
 
-    @MockitoSpyBean
-    private DocumentService documentService;
-
-    private UUID subscriptionId;
-    private UUID callbackDocumentId;
-
     @BeforeEach
     void setUp() {
-        reset(callbackDeliveryService);
+        WireMock.reset();
+        if (nonNull(callbackWireMock)) {
+            callbackWireMock.resetAll();
+        }
         clearAllTables();
     }
 
@@ -80,7 +95,6 @@ class NotificationPcrE2EIntegrationTest extends IntegrationTestBase {
         when_a_pcr_event_is_posted();
         when_material_service_responds();
 
-        then_the_material_api_is_called();
         then_the_subscriber_receives_a_callback();
         then_the_subscriber_can_retrieve_the_document();
     }
@@ -96,59 +110,47 @@ class NotificationPcrE2EIntegrationTest extends IntegrationTestBase {
 
         then_the_material_api_was_polled();
         then_the_subscriber_does_not_receive_a_callback();
-        then_the_subscriber_cannot_retrieve_the_document();
     }
 
     private void given_i_am_a_subscriber_with_a_subscription() throws Exception {
         createSubscription();
     }
 
-    private void given_i_have_a_callback_endpoint() {
-        // Callback endpoint represented by @MockitoBean CallbackDeliveryService
+    private void given_i_have_a_callback_endpoint() throws IOException {
+        stubCallbackEndpoint(callbackWireMock, CALLBACK_URI);
     }
 
-    private void given_material_service_returns_document_success() {
-         /*WireMock (material-client) stubs from wiremock/material-client/mappings/:
-         material-metadata-mapping.json     GET .../material/6c198796-08bb-4803-b456-fa0c29ca6021/metadata  → 200, __files/material-response.json
-         material-content-full-mapping.json GET .../material/6c198796-08bb-4803-b456-fa0c29ca6021/content   → 200, __files/material-with-contenturl.json
-         material-binary-mapping.json       GET .../material/6c198796-08bb-4803-b456-fa0c29ca6021/binary    → 200, __files/material-content.pdf*/
+    private void given_material_service_returns_document_success() throws IOException {
+        stubMaterialMetadata(MATERIAL_ID);
+        stubMaterialContent(MATERIAL_ID);
+        stubMaterialBinary(MATERIAL_ID);
     }
 
     private void given_material_service_returns_document_not_found() {
-         /*Request payload: stubs/requests/progression-pcr/pcr-request-material-timeout.json
-           eventId: 11111111-1111-1111-1111-111111111111, materialId: 11111111-1111-1111-1111-111111111112, eventType: PRISON_COURT_REGISTER_GENERATED
-           WireMock stub (wiremock/material-client/mappings/):
-           material-metadata-timeout-mapping.json  GET .../material/11111111-1111-1111-1111-111111111112/metadata  → 204 (no body)
-           App polls until Awaitility times out → 504 "Material metadata not ready", no save/callback.*/
+        stubMaterialMetadataNoContent(MATERIAL_ID_TIMEOUT);
     }
 
     private void when_a_pcr_event_is_posted() throws Exception {
-        mockMvc.perform(post(NOTIFICATIONS_PCR_URI)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .header("Accept", MediaType.APPLICATION_JSON_VALUE)
-                        .content(loadPayload(PCR_EVENT_PAYLOAD_PATH)))
-                .andDo(print())
-                .andExpect(status().isAccepted());
+        postPcrEvent(PCR_EVENT_PAYLOAD_PATH).andExpect(status().isAccepted());
     }
 
     private void when_a_pcr_event_is_posted_with_timeout() throws Exception {
-        mockMvc.perform(post(NOTIFICATIONS_PCR_URI)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .header("Accept", MediaType.APPLICATION_JSON_VALUE)
-                        .content(loadPayload(PCR_EVENT_TIMEOUT_PATH)))
-                .andDo(print())
+        postPcrEvent(PCR_EVENT_TIMEOUT_PATH)
                 .andExpect(status().isGatewayTimeout())
                 .andExpect(content().string("Material metadata not ready"));
     }
 
-    private void when_material_service_responds() {
-        /* This step does nothing; the material response already happens during when_a_pcr_event_is_posted (app calls MaterialApi, WireMock serves stubs)
-           Success: GET metadata → 200 → save document mapping → callback.
-           Failure: GET metadata → 204 until timeout → 504.*/
+    private ResultActions postPcrEvent(String payloadPath) throws Exception {
+        return mockMvc.perform(post(NOTIFICATIONS_PCR_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Accept", MediaType.APPLICATION_JSON_VALUE)
+                        .content(loadPayload(payloadPath)))
+                .andDo(print());
     }
 
-    private void then_the_material_api_is_called() {
-        verify(materialApi, times(1)).getMaterialMetadataByMaterialId(eq(MATERIAL_ID));
+    private void when_material_service_responds() {
+        await().atMost(Duration.ofSeconds(5))
+                .untilAsserted(() -> verify(materialApi, atLeastOnce()).getMaterialMetadataByMaterialId(any(UUID.class)));
     }
 
     private void then_the_material_api_was_polled() {
@@ -156,19 +158,42 @@ class NotificationPcrE2EIntegrationTest extends IntegrationTestBase {
     }
 
     private void then_the_subscriber_receives_a_callback() {
-        ArgumentCaptor<UUID> documentIdCaptor = ArgumentCaptor.forClass(UUID.class);
-        verify(callbackDeliveryService, times(1))
-                .processPcrEvent(any(PcrEventPayload.class), documentIdCaptor.capture());
-        callbackDocumentId = documentIdCaptor.getValue();
-        assertThat(callbackDocumentId).isNotNull();
+        callbackWireMock.verify(1, postRequestedFor(urlPathEqualTo(CALLBACK_URI)));
+        callbackDocumentId = getDocumentIdFromCallbackServeEvents();
+    }
+
+    private UUID getDocumentIdFromCallbackServeEvents() {
+        return callbackWireMock.getAllServeEvents().stream()
+                .map(ServeEvent::getRequest)
+                .filter(r -> nonNull(r.getUrl()) && r.getUrl().contains(CALLBACK_URI))
+                .map(r -> parseDocumentIdFromBody(r.getBodyAsString()))
+                .findFirst()
+                .orElseThrow(() ->
+                        new AssertionError("Callback request body did not contain documentId"));
+    }
+
+    private UUID parseDocumentIdFromBody(String body) {
+        if (body == null || body.isBlank()) return null;
+        try {
+            return Optional.ofNullable(new ObjectMapper().readTree(body).get("documentId"))
+                    .map(JsonNode::asText)
+                    .map(UUID::fromString)
+                    .orElse(null);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     private void then_the_subscriber_does_not_receive_a_callback() {
-        verify(callbackDeliveryService, times(0)).processPcrEvent(any(PcrEventPayload.class), any());
+        callbackWireMock.verify(0, postRequestedFor(urlPathEqualTo(CALLBACK_URI)));
     }
 
     private void then_the_subscriber_can_retrieve_the_document() throws Exception {
-        mockMvc.perform(get(DOCUMENT_URI, subscriptionId, callbackDocumentId))
+        getDocumentAndExpectPdf(subscriptionId, callbackDocumentId);
+    }
+
+    private void getDocumentAndExpectPdf(UUID subId, UUID docId) throws Exception {
+        mockMvc.perform(get(DOCUMENT_URI, subId, docId))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(header().string("Content-Type", org.hamcrest.Matchers.containsString("application/pdf")))
@@ -176,17 +201,19 @@ class NotificationPcrE2EIntegrationTest extends IntegrationTestBase {
                 .andExpect(content().contentType(MediaType.APPLICATION_PDF));
     }
 
-    private void then_the_subscriber_cannot_retrieve_the_document() {
-        verify(documentService, times(0)).saveDocumentMapping(any(UUID.class), any(EntityEventType.class));
+    private void createSubscription() throws Exception {
+        String callbackUrl = callbackHttpsBaseUrl.endsWith("/") ? callbackHttpsBaseUrl + CALLBACK_URI.substring(1) : callbackHttpsBaseUrl + CALLBACK_URI;
+        String body = loadPayload(SUBSCRIPTION_REQUEST_E2E).replace("{{callback.url}}", callbackUrl);
+        String json = postSubscriptionAndReturnJson(body);
+        subscriptionId = UUID.fromString(new ObjectMapper().readTree(json).get("clientSubscriptionId").asText());
     }
 
-    private void createSubscription() throws Exception {
-        String json = mockMvc.perform(post(CLIENT_SUBSCRIPTIONS_URI)
+    private String postSubscriptionAndReturnJson(String body) throws Exception {
+        return mockMvc.perform(post(CLIENT_SUBSCRIPTIONS_URI)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(loadPayload(SUBSCRIPTION_REQUEST_E2E)))
+                        .content(body))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.clientSubscriptionId").exists())
                 .andReturn().getResponse().getContentAsString();
-        subscriptionId = UUID.fromString(new ObjectMapper().readTree(json).get("clientSubscriptionId").asText());
     }
 }
