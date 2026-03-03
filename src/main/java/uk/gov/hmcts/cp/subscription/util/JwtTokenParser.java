8a01548edd0976b57c654bd3b4fbad01eb9131c4
coling01 <colin.greenwood@hmcts.net>
2026-03-03 06:15:48 +0000

    chore: add logging for clientId auth filter

--- src/main/java/uk/gov/hmcts/cp/subscription/util/JwtTokenParser.java
+++ src/main/java/uk/gov/hmcts/cp/subscription/util/JwtTokenParser.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.cp.subscription.util;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 import uk.gov.hmcts.cp.subscription.services.JsonMapper;
 
 import java.nio.charset.StandardCharsets;
@@ -27,26 +29,35 @@ public class JwtTokenParser {
         try {
             final String authHeader = nonNull(request) ? request.getHeader("Authorization") : null;
             if (isNull(authHeader) || authHeader.isBlank() || !authHeader.startsWith(BEARER_PREFIX)) {
+                log.error("Authorization header is not set or starting with {}", BEARER_PREFIX);
                 return null;
             }
             final String token = authHeader.substring(BEARER_PREFIX.length()).trim();
             if (token.isEmpty()) {
+                log.error("Authorization token is not set");
                 return null;
             }
             final String[] chunks = token.split("\\.");
             if (chunks.length < MAX_NUMBER_OF_CHUNKS) {
+                log.error("Authorization period separated chunks is {} we expected {}", chunks.length, MAX_NUMBER_OF_CHUNKS);
                 return null;
             }
             final Base64.Decoder decoder = Base64.getUrlDecoder();
             final String payloadJson = new String(decoder.decode(chunks[1]), StandardCharsets.UTF_8);
             final UUID clientId = jsonMapper.getUUIDAtPath(payloadJson, AZP_JSON_POINTER);
             if (clientId != null) {
-                log.debug("Extracted client ID {} from JWT azp claim", clientId);
+                log.info("Authorization extracted client ID {} from JWT azp claim", clientId);
             }
             return clientId;
         } catch (Exception e) {
-            log.warn("Failed to extract client ID from JWT token - returning null. Reason: {}", e.getMessage());
+            log.error("Authorization failed to extract client ID from JWT token - returning null. Exception: {}", e.getMessage());
             return null;
         }
+    }
+
+    // We should throw error rather than returning null
+    private void throwError(String errorMessage) {
+        log.error("Authorization token error. {}", errorMessage);
+        throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED, errorMessage);
     }
 }
