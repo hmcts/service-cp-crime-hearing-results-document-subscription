81aad8cdeb728118898355a3926f4b2e5fe04318
coling01 <colin.greenwood@hmcts.net>
2026-03-17 11:51:19 +0000

    feature: correlation-id to material-service need to not use await() so split for async

--- apiTest/src/test/java/uk/gov/hmcts/cp/subscription/http/wiremock/MaterialServiceTest.java
+++ apiTest/src/test/java/uk/gov/hmcts/cp/subscription/http/wiremock/MaterialServiceTest.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.cp.subscription.http.wiremock;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,13 +18,19 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @Slf4j
 class MaterialServiceTest {
-    private final String baseUrl = "http://localhost:8090";
-    private final String CORRELATION_ID_KEY = "X-Correlation-Id";
-    private final RestClient restClient = RestClient.create();
-    private final JsonMapper jsonMapper = new JsonMapper();
+    private String wiremockRequestsUrl = "http://localhost:8090/__admin/requests";
+    private String baseUrl = "http://localhost:8090";
+    private String CORRELATION_ID_KEY = "X-Correlation-Id";
+    private RestClient restClient = RestClient.create();
+    private JsonMapper jsonMapper = new JsonMapper();
 
     UUID correlationId = UUID.randomUUID();
-    UUID materialId = UUID.randomUUID();
+    UUID materialId = UUID.fromString("7998ebac-1bd0-4079-b4ca-1151aa16157c");
+
+    @BeforeEach
+    void beforeEach() {
+        clearWireMockMessages();
+    }
 
     @Test
     void material_metadata_should_return() {
@@ -51,7 +58,7 @@ class MaterialServiceTest {
     }
 
     @Test
-    void material_full_content_with_url_should_return(){
+    void material_full_content_with_url_should_return() {
         ResponseEntity<String> response = restClient
                 .get()
                 .uri(contentUrl(materialId))
@@ -64,6 +71,8 @@ class MaterialServiceTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         String expectedContentUrl = "\"http://localhost:8090/material-query-api/query/api/rest/material/material/6c198796-08bb-4803-b456-fa0c29ca6021/binary\"";
         assertThat(jsonNode.get("contentUrl").toString()).isEqualTo(expectedContentUrl);
+        // TODO add response transformer to correctly set the materialId as per the request
+        assertWireMockMessage("/material-query-api/query/api/rest/material/material/7998ebac-1bd0-4079-b4ca-1151aa16157c/content");
     }
 
     private String metaDataUrl(UUID materialId) {
@@ -76,5 +85,31 @@ class MaterialServiceTest {
         String url = String.format("%s/material-query-api/query/api/rest/material/material/%s/content", baseUrl, materialId);
         log.info("metaDataUrl:{}", url);
         return url;
+    }
+
+    private void clearWireMockMessages() {
+        ResponseEntity<String> response = restClient
+                .delete()
+                .uri(wiremockRequestsUrl)
+                .retrieve()
+                .toEntity(String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    private void assertWireMockMessage(String expectedUrl) {
+        ResponseEntity<String> response = restClient
+                .get()
+                .uri(wiremockRequestsUrl)
+                .retrieve()
+                .toEntity(String.class);
+        log.info("Wiremock {}", response.getBody());
+
+        int requestCount = jsonMapper.getIntAtPath(response.getBody(), "/meta/total");
+        String lastUrl = jsonMapper.getStringAtPath(response.getBody(), String.format("/requests/%d/request/url", requestCount - 1));
+        String lastCorrelationId = jsonMapper.getStringAtPath(response.getBody(), String.format("/requests/%d/request/headers/x-correlation-id", requestCount - 1));
+        int lastStatus = jsonMapper.getIntAtPath(response.getBody(), String.format("/requests/%d/response/status", requestCount - 1));
+        assertThat(lastUrl).isEqualTo(expectedUrl);
+        assertThat(lastStatus).isEqualTo(200);
+        assertThat(lastCorrelationId).isNotEmpty();
     }
 }
