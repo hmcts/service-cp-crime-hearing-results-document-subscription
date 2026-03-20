402ed19fdf0cdd738be2c9df6974cdad9f42d2da
coling01 <colin.greenwood@hmcts.net>
2026-03-20 14:48:09 +0000

    debug: add test to help debug signature issue

--- src/test/java/uk/gov/hmcts/cp/hmac/services/HmacSigningServiceTest.java
+++ src/test/java/uk/gov/hmcts/cp/hmac/services/HmacSigningServiceTest.java
@@ -31,11 +31,15 @@ public class HmacSigningServiceTest {
 
     @Test
     void real_signature_and_message_should_match() throws InvalidKeyException {
+        // Message sent at 14:30:07
         KeyPair keyPair = hmacKeyService.generateKey();
         String message = "{\"cases\":[{\"urn\":\"AAA20713684\"}],\"masterDefendantId\":\"d5ad03d0-5f2d-477b-9aa8-e0beb68f46a3\",\"defendantName\":\"Sarogav Noem\",\"defendantDateOfBirth\":\"2008-12-08\",\"documentId\":\"6deab43e-70f2-4d8e-bc28-04ab39fbeff1\",\"documentGeneratedTimestamp\":\"2026-03-20T14:29:15.453071625Z\",\"prisonEmailAddress\":\"yoiashfield.premiercustody@premier-serco.cjsm.net\"}";
         String signature = hmacSigningService.sign(keyPair.getSecret(), message);
         hmacSigningService.validateSignature(keyPair.getSecret(), message, signature);
         assertThat(signature).isEqualTo("mevCvmfiXXoHoeRbvgat8PtHjnAvOgnXMMUgZ+BKmTE=");
+
+        String encodedSecret = encodingService.encodeWithBase64(keyPair.getSecret());
+        assertThat(encodedSecret).isEqualTo("U3R1YiBzdHJpbmcgdXNlZCBwdXJlbHkgZm9yIGRldmVsb3BtZW50IHB1cnBvc2VzLiBUbyBiZSBzZWN1cmVkLg==");
     }
 
     @Test
