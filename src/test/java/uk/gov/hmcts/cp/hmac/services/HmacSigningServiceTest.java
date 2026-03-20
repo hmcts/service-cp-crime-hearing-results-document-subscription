f075029439cd1ebe4075ca25e77183aeb7554212
coling01 <colin.greenwood@hmcts.net>
2026-03-20 14:36:17 +0000

    debug: add test to help debug signature issue

--- src/test/java/uk/gov/hmcts/cp/hmac/services/HmacSigningServiceTest.java
+++ src/test/java/uk/gov/hmcts/cp/hmac/services/HmacSigningServiceTest.java
@@ -24,8 +24,19 @@ public class HmacSigningServiceTest {
     HmacKeyService hmacKeyService;
     @InjectMocks
     HmacSigningService hmacSigningService;
+    @InjectMocks
+    EncodingService encodingService;
 
     String message = "A message";
+
+    @Test
+    void real_signature_and_message_should_match() throws InvalidKeyException {
+        KeyPair keyPair = hmacKeyService.generateKey();
+        String message = "{\"cases\":[{\"urn\":\"AAA20713684\"}],\"masterDefendantId\":\"d5ad03d0-5f2d-477b-9aa8-e0beb68f46a3\",\"defendantName\":\"Sarogav Noem\",\"defendantDateOfBirth\":\"2008-12-08\",\"documentId\":\"6deab43e-70f2-4d8e-bc28-04ab39fbeff1\",\"documentGeneratedTimestamp\":\"2026-03-20T14:29:15.453071625Z\",\"prisonEmailAddress\":\"yoiashfield.premiercustody@premier-serco.cjsm.net\"}";
+        String signature = hmacSigningService.sign(keyPair.getSecret(), message);
+        hmacSigningService.validateSignature(keyPair.getSecret(), message, signature);
+        assertThat(signature).isEqualTo("mevCvmfiXXoHoeRbvgat8PtHjnAvOgnXMMUgZ+BKmTE=");
+    }
 
     @Test
     void validate_should_verify_ok() throws InvalidKeyException {
