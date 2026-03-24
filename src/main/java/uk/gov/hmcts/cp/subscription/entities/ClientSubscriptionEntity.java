357299a03183b9aa9e3848b2890ffcce76a7b5aa
vineetkapoor-hmcts <vineet.kapoor@hmcts.net>
2026-03-24 13:11:40 +0000

    AMP-197 Updated datatype for EventType (Part-2)

--- src/main/java/uk/gov/hmcts/cp/subscription/entities/ClientSubscriptionEntity.java
+++ src/main/java/uk/gov/hmcts/cp/subscription/entities/ClientSubscriptionEntity.java
@@ -2,8 +2,6 @@ package uk.gov.hmcts.cp.subscription.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
