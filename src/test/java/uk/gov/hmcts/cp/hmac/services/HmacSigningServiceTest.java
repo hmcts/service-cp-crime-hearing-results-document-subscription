1fa045e1c2963b541e94cad8186cbb0ebddaa18c
coling01 <colin.greenwood@hmcts.net>
2026-04-01 13:30:14 +0100

    chore: added debug for callback hmac issue
    * Rename CorrelationIdService to UUIDService
    * Use UUIDService to generate documentId in e2e tests to get repeatability
    * Made document mapper accept documentId and now() not generated
    * Capture hmac in e2e test when create subscription
    * Verify signature on callback payload in e2e test

--- src/test/java/uk/gov/hmcts/cp/hmac/services/HmacSigningServiceTest.java
+++ src/test/java/uk/gov/hmcts/cp/hmac/services/HmacSigningServiceTest.java
@@ -35,8 +35,8 @@ public class HmacSigningServiceTest {
     @Test
     void real_signature_and_message_should_match() throws InvalidKeyException {
         String encodedSecret = "insert-encoded-secret-here";
-        String messageJson = "{\"cases\":[{\"urn\":\"AAA20713684\"}],\"masterDefendantId\":\"d5ad03d0-5f2d-477b-9aa8-e0beb68f46a3\",\"defendantName\":\"Sarogav Noem\",\"defendantDateOfBirth\":\"2008-12-08\",\"documentId\":\"02bf6f49-1960-4069-b87c-44cbf91ddeb8\",\"documentGeneratedTimestamp\":\"2026-04-01T10:48:15.561656414Z\",\"prisonEmailAddress\":\"yoiashfield.premiercustody@premier-serco.cjsm.net\"}";
-        String signature = "RH3ua5RLwusmG5/ngPiYvse8n+qYWVIwUQdaLKj3OOc=";
+        String messageJson = "{insert-json-here}";
+        String signature = "signature-here";
         byte[] secret = encodingService.decodeFromBase64(encodedSecret);
 
         hmacSigningService.validateSignature(secret, messageJson, signature);
