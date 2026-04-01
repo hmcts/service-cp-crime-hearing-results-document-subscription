42a9aa58ec796d706f7741a5b00616012a2031a2
coling01 <colin.greenwood@hmcts.net>
2026-04-01 16:16:41 +0100

    chore: extended e2e sync test for hmac signature
    Rename CorrelationIdService to UUIDService
    Use UUIDService to generate documentId in e2e tests to get repeatability
    Made document mapper accept parameters of documentId and now() rather than generated
    Capture hmac in e2e test when create subscription
    Verify signature on callback payload in e2e test

--- src/test/java/uk/gov/hmcts/cp/subscription/integration/IntegrationTestBase.java
+++ src/test/java/uk/gov/hmcts/cp/subscription/integration/IntegrationTestBase.java
@@ -34,7 +34,7 @@ import java.util.UUID;
 
 
 @SpringBootTest
-// @ContextConfiguration(initializers = TestContainersInitialise.class)
+@ContextConfiguration(initializers = TestContainersInitialise.class)
 @AutoConfigureMockMvc
 @Slf4j
 @TestPropertySource(properties = {
