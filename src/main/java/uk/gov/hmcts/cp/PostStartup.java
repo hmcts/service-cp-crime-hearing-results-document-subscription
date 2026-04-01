b9b7958d6d991e1362aca68d72445ea82d3eef2b
srivanimuddineni <srivanimuddineni@gmail.com>
2026-04-01 13:39:25 +0100

    Queues verfiy (#189)

--- src/main/java/uk/gov/hmcts/cp/PostStartup.java
+++ src/main/java/uk/gov/hmcts/cp/PostStartup.java
@@ -4,6 +4,7 @@ import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.messaging.servicebus.administration.ServiceBusAdministrationClient;
 import com.azure.messaging.servicebus.administration.ServiceBusAdministrationClientBuilder;
 import com.azure.messaging.servicebus.administration.models.CreateQueueOptions;
+import com.azure.messaging.servicebus.administration.models.QueueProperties;
 import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,7 @@ import uk.gov.hmcts.cp.subscription.repositories.EventTypeRepository;
 import uk.gov.hmcts.cp.vault.VaultServiceProperties;
 
 import java.time.Duration;
+import java.util.List;
 
 /**
  * We add any debug logging we might need such as database checks
@@ -38,23 +40,23 @@ public class PostStartup {
             final String endpoint = configService.getConnectionString();
             log.info("PostStartup service bus connecting for admin client via Entra ID endpoint:{} clientId:{}",
                     endpoint, vaultServiceProperties.getVaultClientId());
-             new ServiceBusAdministrationClientBuilder()
+             final ServiceBusAdministrationClient adminClient = new ServiceBusAdministrationClientBuilder()
                     .endpoint(endpoint)
                     .credential(new DefaultAzureCredentialBuilder()
                             .managedIdentityClientId(vaultServiceProperties.getVaultClientId().toString())
                             .build())
                     .buildClient();
             log.info("SKIPPING PostStartup service bus listing queues");
-            // COLING temp removed this as it hangs in pipelines
-            // We see the Azure auth trying but failing
-            //
-//            final List<String> queues = adminClient.listQueues().stream().map(QueueProperties::getName).toList();
-//            log.info("PostStartup service bus has queues:{}", queues);
-//            for (final String queue : queues) {
-//                log.info("PostStartup found service bus queue {}", queue);
-//            }
-//            log.info("PostStartup service bus creating queue");
-//            createQueue(adminClient, "hces.notifications.inbound");
+             //COLING temp removed this as it hangs in pipelines
+             //We see the Azure auth trying but failing
+            
+            final List<String> queues = adminClient.listQueues().stream().map(QueueProperties::getName).toList();
+            log.info("PostStartup service bus has queues:{}", queues);
+            for (final String queue : queues) {
+                log.info("PostStartup found service bus queue {}", queue);
+            }
+            log.info("PostStartup service bus creating queue");
+            createQueue(adminClient, "hces.notifications.inbound");
         } catch (Exception e) {
             log.error("PostStartup service bus error.", e);
         }
