ccdcb376f7931743f1b6addf4f317dcf48a04b04
coling01 <colin.greenwood@hmcts.net>
2026-04-02 16:21:16 +0100

    chore: reinstate integration tests

--- src/test/java/uk/gov/hmcts/cp/subscription/integration/e2e/NotificationSynchronousE2EIntegrationTest.java
+++ src/test/java/uk/gov/hmcts/cp/subscription/integration/e2e/NotificationSynchronousE2EIntegrationTest.java
@@ -123,7 +123,7 @@ class NotificationSynchronousE2EIntegrationTest extends IntegrationTestBase {
 
     @Test
     void document_retrieval_success_should_return_signed_pdf() throws Exception {
-        // given_i_create_a_new_subscription();
+        given_i_create_a_new_subscription();
         given_i_have_a_callback_endpoint();
         given_material_service_returns_document_success();
 
