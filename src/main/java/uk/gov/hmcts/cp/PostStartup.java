5250cbfa2f8e71a10d70ea6c9b6a7ce967509a5b
Colin Greenwood <Colin.Greenwood@hmcts.net>
2026-04-14 16:02:53 +0100

    AMP-445 Add startup diagnostics: dead letter queue sizes and document/environment logging (#219)
    * chore: debug logging of documents / materials
    
    Also drop unused repository method findByMaterialId
    And make document service findByDocumentId not confusing findById

--- src/main/java/uk/gov/hmcts/cp/PostStartup.java
+++ src/main/java/uk/gov/hmcts/cp/PostStartup.java
@@ -1,9 +1,12 @@
 package uk.gov.hmcts.cp;
 
+import com.azure.messaging.servicebus.administration.ServiceBusAdministrationClient;
+import com.azure.messaging.servicebus.administration.models.QueueRuntimeProperties;
 import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.cp.servicebus.config.ServiceBusProperties;
 import uk.gov.hmcts.cp.subscription.config.AppProperties;
 import uk.gov.hmcts.cp.subscription.repositories.DocumentMappingRepository;
 import uk.gov.hmcts.cp.subscription.repositories.EventTypeRepository;
@@ -20,11 +23,28 @@ public class PostStartup {
     private AppProperties appProperties;
     private EventTypeRepository eventTypeRepository;
     private DocumentMappingRepository documentMappingRepository;
+    private ServiceBusAdministrationClient administrationClient;
 
     @PostConstruct
     public void postStartupLogging() {
         log.info("PostStartup Database contains {} eventTypes", eventTypeRepository.count());
         logRecentDocumentMappings();
+        logDeadLetterQueueSizes();
+    }
+
+    private void logDeadLetterQueueSizes() {
+        logDeadLetterCount(ServiceBusProperties.NOTIFICATIONS_INBOUND_QUEUE);
+        logDeadLetterCount(ServiceBusProperties.NOTIFICATIONS_OUTBOUND_QUEUE);
+    }
+
+    private void logDeadLetterCount(final String queueName) {
+        try {
+            final QueueRuntimeProperties runtimeProperties = administrationClient.getQueueRuntimeProperties(queueName);
+            log.info("PostStartup Queue {} deadLetterMessageCount:{} activeMessageCount:{}",
+                    queueName, runtimeProperties.getDeadLetterMessageCount(), runtimeProperties.getActiveMessageCount());
+        } catch (Exception e) {
+            log.warn("PostStartup Failed to get dead letter count for queue {}: {}", queueName, e.getMessage());
+        }
     }
 
     private void logRecentDocumentMappings() {
